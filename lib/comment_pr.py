cc577a1e2002d79f43b56aa5d8842452a5f6ac58
Abdel Sandoval <abdel.sandoval@unholster.com>
2020-02-25 10:19:31 -0300

    change comment

--- lib/comment_pr.py
+++ lib/comment_pr.py
@@ -8,4 +8,4 @@ pr_number = os.environ["GITHUB_REF"].split("/")[2]
 g = Github(github_token)
 repo = g.get_repo(github_repository)
 pr = repo.get_pull(int(pr_number))
-pr.create_issue_comment("Please run `yapf` before opening a PR")
+pr.create_issue_comment("`Yapf` has formatted your code and committed this changes.")
