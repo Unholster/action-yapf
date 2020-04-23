6bdeb425c3090391d5ee154b0ec66c0861db00c5
Eduardo Escobar <eduardo.escobar@unholster.com>
2020-04-23 15:53:42 -0400

    Cambio comilla doble a simple.

--- lib/comment_pr.py
+++ lib/comment_pr.py
@@ -2,11 +2,11 @@ import os
 
 from github import Github
 
-github_token = os.environ['GITHUB_TOKEN']
-github_repository = os.environ['GITHUB_REPOSITORY']
-pr_number = os.environ['GITHUB_REF'].split('/')[2]
+github_token = os.environ["GITHUB_TOKEN"]
+github_repository = os.environ["GITHUB_REPOSITORY"]
+pr_number = os.environ["GITHUB_REF"].split("/")[2]
 
 g = Github(github_token)
 repo = g.get_repo(github_repository)
 pr = repo.get_pull(int(pr_number))
-pr.create_issue_comment('`Yapf` has formatted your code and committed this changes.')
+pr.create_issue_comment("`Yapf` has formatted your code and committed this changes.")
